befcfc11d579bced504c363af31acecd21acbf36
Jacob-Jake-Kwon <kwonj0115@gmail.com>
2025-04-11 23:33:54 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import './globals.css';
 
 export const metadata = {
-  title: 'Jake의 추천 링크',
-  description: '써보고 괜찮았던 것만 모았어요!',
+  title: '연예인들의 꿀템 추천 링크',
+  description: '연예인들이 직접 추천한 꿀템들만 모았어요!',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
