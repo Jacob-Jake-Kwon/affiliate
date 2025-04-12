3c18e978f5de10ef1edcafd6802dc8ab4294754e
Jacob-Jake-Kwon <kwonj0115@gmail.com>
2025-04-12 12:01:28 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -133,7 +133,9 @@ export default function Home() {
                 <h2 className="text-lg font-medium">{link.title}</h2>
               </div>
               <p className="text-sm text-gray-600 dark:text-gray-300">{link.description}</p>
-
+              <p className="text-xs text-gray-400 dark:text-gray-500">
+                이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다.
+              </p>
               <div className="flex gap-2 items-center">
                 <Button asChild>
                   <a
